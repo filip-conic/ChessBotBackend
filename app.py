df60ce859fd636440515139bdbbbfb42e0a7d2d0
filip-conic <filipconic77@gmail.com>
2024-10-28 14:58:09 -0700

    Backend called for moves by frontend working

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
 from Engine.HomemadeChessEngine import get_best_move
 import chess
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 in_memory_datastore = {
    "COBOL" : {"name": "COBOL", "publication_year": 1960, "contribution": "record data"},
@@ -10,10 +14,12 @@ in_memory_datastore = {
 }
 
 @app.get('/programming_languages')
+@cross_origin()
 def list_programming_languages():
    return {"programming_languages":list(in_memory_datastore.values())}
 
 @app.route('/testMove', methods=['POST'])
+@cross_origin()
 def testMove():
     print("TEST MOVE CALLED")
     data = request.get_json()
@@ -21,3 +27,13 @@ def testMove():
     board = chess.Board(board_fen)
     move = get_best_move(board, board.turn)
     return jsonify({'best_move': str(move)})
+
+@app.route('/echo', methods=['POST'])
+@cross_origin()
+def echo():
+    data = request.get_json()  # Get JSON data from the request
+    print("HELLO")
+    print(data)
+
+    field = data.get('field')  # Extract the 'field' from the JSON data
+    return jsonify({'field': field})  # Return the same field in the response
