fc490b4d3b367a1a5eeb161020da8be5fd7c6f39
filip-conic <filipconic77@gmail.com>
2024-12-10 17:32:53 -0800

    Updating with full version of AI

---
+++ HomemadeEngine/ChessEngine.py
@@ -0,0 +1,125 @@
+import chess
+from HomemadeEngine.Evaluation import evaluate_position, negamax_evaluate_position
+from HomemadeEngine.EngineConstants import *
+
+nodes = 0
+nodeCount = 1
+nodePrinted = True
+
+print_depth_ls = [False for i in range(100)]
+
+def order_moves(board):
+    legal_moves = board.legal_moves
+    captures = []
+    checks = []
+    other = []
+
+    for move in legal_moves:
+        if board.is_capture(move):
+            captures.append(move)
+        elif board.gives_check(move):
+            checks.append(move)
+        else:
+            other.append(move)
+
+    ordered_moves = checks + captures + other
+    return ordered_moves
+
+# NEGAMAX IMPLEMENTATION
+def negamax(board, depth, alpha, beta, use_quiet_search):    
+    if depth <= 0 or board.is_game_over():
+        if use_quiet_search:
+            return negamax_quiescence_search(board, alpha, beta, 1, 10)
+        else:
+            return negamax_evaluate_position(board)
+    
+    global nodes
+    nodes += 1
+    best_move = -1000
+    ordered_moves = order_moves(board)
+
+    global nodeCount
+    global nodePrinted
+    if (nodes > nodeCount * 100000):
+        nodePrinted = False
+    if not nodePrinted:
+        print("Current node num: " + str(nodes))
+        nodePrinted = True
+        nodeCount += 1
+
+
+    for move in ordered_moves:
+        board.push(move)
+        eval = -negamax(board, depth-1, -beta, -alpha, use_quiet_search)
+        board.pop()
+
+        best_move = max(best_move, eval)
+        alpha = max(alpha, eval)
+        if alpha >= beta:
+            break
+    return best_move
+
+def negamax_get_best_move(board, depth=3, use_quiet_search=True):
+        alpha = -1000
+        beta = 1000
+
+        best_move_val = -100
+        best_move = None
+        global nodes
+        nodes = 0
+
+        ordered_moves = order_moves(board)
+        print("Use Quiet Search: " + str(use_quiet_search))
+
+        for move in ordered_moves:
+            board.push(move)
+            eval = -negamax(board, depth-1, alpha, beta, use_quiet_search)
+            board.pop()
+
+            if eval > best_move_val:
+                best_move_val = eval
+                best_move = move
+        
+        print("Nodes for move search: " + str(nodes))
+        return best_move
+
+# Searches all captures
+def negamax_quiescence_search(board, alpha, beta, depth, maxDepth):
+    if maxDepth <= 0 or board.is_game_over():
+        return negamax_evaluate_position(board)
+
+    eval = negamax_evaluate_position(board)
+    alpha = max(eval, alpha)
+
+    if(not print_depth_ls[depth]):
+        print("Hit Depth " + str(depth) + " in quiet search")
+        print_depth_ls[depth] = True
+
+    global nodes
+    nodes += 1
+
+    global nodeCount
+    global nodePrinted
+    if (nodes > nodeCount * 100000):
+        nodePrinted = False
+    if not nodePrinted:
+        print("Current node num: " + str(nodes))
+        nodePrinted = True
+        nodeCount += 1
+
+    if eval >= beta:
+        return beta
+    
+    ordered_moves = order_moves(board)
+    
+    for move in ordered_moves:
+        if board.is_capture(move) or board.gives_check(move):
+            board.push(move)
+            eval = -negamax_quiescence_search(board, -beta, -alpha, depth+1, maxDepth-1)
+            board.pop()
+
+            alpha = max(alpha, eval)
+            if eval >= beta:
+                return beta
+            
+    return alpha
--- app.py
+++ app.py
@@ -2,21 +2,11 @@ from flask import Flask, request, jsonify
 from Engine.HomemadeChessEngine import get_best_move
 import chess
 from flask_cors import CORS, cross_origin
+from HomemadeEngine.ChessEngine import negamax_get_best_move
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-in_memory_datastore = {
-   "COBOL" : {"name": "COBOL", "publication_year": 1960, "contribution": "record data"},
-   "ALGOL" : {"name": "ALGOL", "publication_year": 1958, "contribution": "scoping and nested functions"},
-   "APL" : {"name": "APL", "publication_year": 1962, "contribution": "array processing"},
-}
-
-@app.get('/programming_languages')
-@cross_origin()
-def list_programming_languages():
-   return {"programming_languages":list(in_memory_datastore.values())}
 
 @app.route('/testMove', methods=['POST'])
 @cross_origin()
@@ -25,8 +15,32 @@ def testMove():
     data = request.get_json()
     board_fen = data.get('board_fen')
     board = chess.Board(board_fen)
-    move = get_best_move(board, board.turn)
-    return jsonify({'best_move': str(move)})
+    best_move = negamax_get_best_move(board, 3)
+    print("Best Move: " + str(best_move))
+    return jsonify({'best_move': str(best_move)})
+
+@app.route('/testGet', methods=['GET'])
+@cross_origin()
+def testGet():
+    print("TEST GET CALLED")
+    # data = request.get_json()
+    # board_fen = data.get('board_fen')
+    # board = chess.Board(board_fen)
+    # best_move = negamax_get_best_move(board, 3)
+    best_move = "TEST GET CALLED"
+    print("Best Move: " + str(best_move))
+    return jsonify({'best_move': str(best_move)})
+
+@app.route('/calculateBestMove', methods=['POST'])
+@cross_origin()
+def calculateBestMove():
+    print("Calculate Best Move Called")
+    data = request.get_json()
+    board_fen = data.get('board_fen')
+    board = chess.Board(board_fen)
+    best_move = negamax_get_best_move(board, 3, use_quiet_search=False)
+    print("Best Move: " + str(best_move) + "\n")
+    return jsonify({'best_move': str(best_move)})
 
 @app.route('/echo', methods=['POST'])
 @cross_origin()
